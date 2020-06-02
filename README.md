b0f753c4f25927886c1c9d791682343da180addc
Haider Ali Punjabi <haiderali@cyberservices.com>
2020-06-02 16:12:12 +0530

    added readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# Harry Potter Fanfiction Analysis
+
+Repository with code to generate visualisations of Harry Potter Fanfiction and Books
+
+Read More about it on my blog:  
+[Harry Potter Books & Fanfiction - An Analysis of Words
+](https://blog.haideralipunjabi.com/posts/harry-potter-books-fanfiction-an-analysis-of-words/)
--- make_wordcloud.py
+++ make_wordcloud.py
@@ -5,15 +5,15 @@ import numpy as np
 from PIL import Image
 import json
 
-mask = np.array(Image.open("masks/book7.png"))
-data = json.load(open("bookdata/book7.json","r"))
+# mask = np.array(Image.open("masks/book7.png"))
+data = json.load(open("extra/genre.json","r"))
 
-print(f"Diffrent Words: {len(data.keys())} | Total Words: {sum(data.values())}")
-wc = WordCloud(width=1695,height=1695,background_color="white", max_words=2000, mask=mask,
+# print(f"Diffrent Words: {len(data.keys())} | Total Words: {sum(data.values())}")
+wc = WordCloud(width=512,height=512,background_color="white", max_words=2000,
                max_font_size=40, random_state=42,contour_width=1)
 
 wc.generate_from_frequencies(data)
 s = wc.to_svg()
-print(s,file=open("out/book7.svg","w"))
+print(s,file=open("out/genre.svg","w"))
 
 
